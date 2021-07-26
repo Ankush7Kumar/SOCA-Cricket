3536c8b26897a146d413cc29f4928208d968ea64
Ankush Kumar <j268sing@edu.uwaterloo.ca>
2021-07-26 11:51:39 -0400

    image fix

--- src/views/Sponsors.js
+++ src/views/Sponsors.js
@@ -98,7 +98,7 @@ const Sponsors = ({
         <Image
           className="has-shadow"
           className="to-fill-image"
-          src={require('./../assets/images/punjabtax.JPEG')}
+          src={require('./../assets/images/punjabtax.jpg')}
           alt="Unable to load image"
           width={200}
           height={200} />
@@ -193,7 +193,7 @@ const Sponsors = ({
         <Image
           className="has-shadow"
           className="to-fill-image"
-          src={require('./../assets/images/punjabtax.JPEG')}
+          src={require('./../assets/images/punjabtax.jpg')}
           alt="Unable to load image"
           width={200}
           height={200} />
