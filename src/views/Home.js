b4f41a1db8e3c48598c0f59945f0e53d19622a17
Chandru Muthukumaravel <chan1987@Chandrus-MacBook-Pro.local>
2022-09-30 09:17:03 -0400

    adding new sponsors

--- src/views/Home.js
+++ src/views/Home.js
@@ -293,6 +293,45 @@ const Home = ({
 
       <div className="aline-horizontal-sponsor to-flex-sponsor" style={{ justifyContent: 'center' }}>
 
+      <a href="https://www.yyzairportlimousine.ca/" target="_blank" className="to-flex p-2" >
+
+
+<Image
+  className="has-shadow"
+  className="to-fill-image"
+  src={require('./../assets/images/airportcar.jpg')}
+  alt="Unable to load image"
+  width={200}
+  height={200} />
+
+</a>
+
+<a href="https://royalindianbuffet.com/" target="_blank" className="to-flex p-2" >
+
+
+<Image
+className="has-shadow"
+className="to-fill-image"
+src={require('./../assets/images/eat.jpg')}
+alt="Unable to load image"
+width={200}
+height={200} />
+
+</a>
+
+<a href="https://www.ayazkarim.ca/" target="_blank" className="to-flex p-2" >
+
+
+<Image
+className="has-shadow"
+className="to-fill-image"
+src={require('./../assets/images/homelife.jpg')}
+alt="Unable to load image"
+width={200}
+height={200} />
+
+</a>
+
         
 
         <a href="https://www.TeamHomeRealty.com/" target="_blank" className="to-flex p-2" >
